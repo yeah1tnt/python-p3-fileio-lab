b40e5c6c21f2717e3616add8f5c918275b68a8c4
Tong Tang <pichip22@hotmail.com>
2023-08-13 13:56:31 -0400

    completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    file = open(f'{file_name}.txt', 'w',encoding='utf-8')
+    file.write(file_content)
+    file.close()
 
 def append_file(file_name, append_content):
-    pass
+    file = open(f'{file_name}.txt', 'a',encoding='utf-8')
+    file.write(append_content)
+    file.close()
 
 def read_file(file_name):
-    pass
+    file = open(f'{file_name}.txt', 'r',encoding='utf-8')
+    file_content = file.read()
+    file.close()
+    return file_content
